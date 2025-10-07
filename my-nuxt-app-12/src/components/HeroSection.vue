71eafbfc6a1ad4d93ed99c984581f8dfea3e8a0d
HoangAn147 <hoangan11ab4@gmail.com>
2025-10-07 12:57:39 +0700

    complete 12

---
+++ my-nuxt-app-12/src/components/HeroSection.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="wrapper bg-light">
+    <div class="container pt-10 pt-md-14 pb-14 pb-md-16 text-center">
+      <div class="row gx-lg-8 gx-xl-12 gy-10 gy-xl-0 mb-14 align-items-center">
+        <div class="col-lg-7 order-lg-2">
+          <figure>
+            <img class="img-auto" :src="i21" :srcset="i21x2 + ' 2x'" alt="Illustration">
+          </figure>
+        </div>
+
+        <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-5 text-center text-lg-start">
+          <h1 class="display-1 fs-54 mb-5 mx-md-n5 mx-lg-0 mt-7">
+          <span class="d-block">A digital agency</span>
+          <span class="d-block">specializing on</span>
+          <span ref="rotatorEl" class="rotator-fade text-primary d-block"></span>
+        </h1>
+          <p class="lead fs-lg mb-7">
+            We are an award winning design agency that strongly believes in the power of creative ideas.
+          </p>
+          <span><a class="btn btn-lg btn-primary rounded-pill me-2">Get Started</a></span>
+        </div>
+      </div>
+      </div>
+  </section>
+</template>
+
+<script setup>
+import i21   from '../assets/img/illustrations/i21.png'
+import i21x2 from '../assets/img/illustrations/i21@2x.png'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { startTextRotator } from '@/utils/ui'
+
+const modules = import.meta.glob('../assets/img/brands/*.png', { eager: true, import: 'default' })
+const brands = Object.values(modules)
+
+// rotator
+const rotatorEl = ref(null)
+let stopRotator = () => {}
+
+onMounted(() => {
+  if (rotatorEl.value) {
+    stopRotator = startTextRotator(
+      rotatorEl.value,
+      ['mobile design', 'web design', '3D animation'],
+      2500
+    )
+  }
+})
+
+onBeforeUnmount(() => {
+  stopRotator && stopRotator()
+})
+</script>
---
+++ my-nuxt-app-12/src/pages/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useHomepageStore } from '@/stores/useItemStore'
+import { useSeoMeta } from '#imports'
+
+const homepageStore = useHomepageStore()
+
+onMounted(() => {
+  homepageStore.fetchHomepageSections()
+})
+
+useSeoMeta({
+  title: 'Trang Chủ - My App',
+  description: 'Xem phim HD miễn phí, tốc độ cao',
+  ogTitle: 'My App Movie',
+  ogDescription: 'Kho phim online hấp dẫn',
+  ogType: 'website'
+})
+</script>
+
+<template>
+  <div class="content-wrapper">
+    <ClientOnly><NavCollapse /></ClientOnly>
+    <HeroSection v-if="homepageStore.heroData" :my_hero="homepageStore.heroData" />
+
+    <div class="text-center my-5">
+      <NuxtLink to="/movies" class="btn btn-primary mx-2">🎬 Xem Phim</NuxtLink>
+      <NuxtLink to="/thiet-ke-website" class="btn btn-outline-primary mx-2">
+        💻 Dịch vụ Thiết kế Website
+      </NuxtLink>
+    </div>
+
+    <ClientsSection v-if="homepageStore.clientsData?.length" :my_client="homepageStore.clientsData" />
+    <ServicesSection v-if="homepageStore.servicesData?.length" :my_services="homepageStore.servicesData" />
+    <ProcessSection v-if="homepageStore.processData" :my_process="homepageStore.processData" />
+    <SolutionsSection v-if="homepageStore.solutionsData?.length" :my_solution="homepageStore.solutionsData" />
+    <TeamSection v-if="homepageStore.teamData?.length" :my_team="homepageStore.teamData" />
+    <ProjectsSection v-if="homepageStore.projectsData?.length" :my_project="homepageStore.projectsData" />
+    <TestimonialsSection v-if="homepageStore.testimonialsData?.length" :my_testimonials="homepageStore.testimonialsData" />
+    <FaqSection v-if="homepageStore.faqData?.length" :my_faq="homepageStore.faqData" />
+    <FooterSection />
+
+
+  </div>
+</template>
